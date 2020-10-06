39f678d7fd5d5e223586cfb62c400644e4a39952
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-10-06 12:10:40 +0500

    Searach Option has been enabled

--- app/src/main/java/mrperfect/example/covidtracker/MyCustomAdapter.java
+++ app/src/main/java/mrperfect/example/covidtracker/MyCustomAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,12 +14,13 @@ import androidx.annotation.Nullable;
 
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class MyCustomAdapter extends ArrayAdapter<CountryModel> {
     private Context context;
-    private List<CountryModel> countryModesList;
-    private List<CountryModel> countryModesListFiltered;
+    private List<CountryModel> countryModelsList;
+    private List<CountryModel> countryModelsListFiltered;
 
 
 
@@ -26,8 +28,8 @@ class MyCustomAdapter extends ArrayAdapter<CountryModel> {
         super(context, R.layout.list_custom_item, countryModesList);
 
         this.context = context;
-        this.countryModesList = countryModesList;
-        this.countryModesListFiltered = countryModesList;
+        this.countryModelsList = countryModesList;
+        this.countryModelsListFiltered = countryModesList;
     }
 
     @NonNull
@@ -37,20 +39,67 @@ class MyCustomAdapter extends ArrayAdapter<CountryModel> {
         TextView tvCountryName = view.findViewById(R.id.tvCountryName);
         ImageView imageView = view.findViewById(R.id.imageFlag);
 
-        tvCountryName.setText(countryModesListFiltered.get(position).getCountry());
-        Glide.with(context).load(countryModesListFiltered.get(position).getFlag()).into(imageView);
+        tvCountryName.setText(countryModelsListFiltered.get(position).getCountry());
+        Glide.with(context).load(countryModelsListFiltered.get(position).getFlag()).into(imageView);
 
         return view;
     }
 
     @Override
     public int getCount() {
-        return countryModesListFiltered.size();
+        return countryModelsListFiltered.size();
     }
 
     @Nullable
     @Override
     public CountryModel getItem(int position) {
-        return countryModesListFiltered.get(position);
+        return countryModelsListFiltered.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @NonNull
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults filterResults = new FilterResults();
+                if (constraint ==null || constraint.length() == 0) {
+                    filterResults.count = countryModelsList.size();
+                    filterResults.values = countryModelsList;
+
+                }else {
+                    List<CountryModel> resultsModel = new ArrayList<>();
+                    String searchStr = constraint.toString().toLowerCase();
+
+                    for (CountryModel itemsModel:countryModelsList) {
+    if (itemsModel.getCountry().toLowerCase().contains(searchStr)) {
+                            resultsModel.add(itemsModel);
+    }
+                    }
+
+                    filterResults.count = resultsModel.size();
+                    filterResults.values = resultsModel;
+
+                }
+
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                countryModelsListFiltered = (List<CountryModel>) results.values;
+                AffectedCountries.countryModelsList = (List<CountryModel>) results.values;
+                notifyDataSetChanged();
+
+            }
+        };
+
+
+        return filter;
     }
 }
